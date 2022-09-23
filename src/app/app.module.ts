0cf42c56b63160a308ab9b89ba9569ddfc429c85
grivers24 <grivers24@gmail.com>
2022-09-23 13:28:15 -0400

    lunch break

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule  } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -8,11 +9,11 @@ import { OrganizationsComponent } from './organizations/organizations.component'
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 
-import { HeaderMasterComponent } from './shared/header-master/header-master.component';
-import { FooterMasterComponent } from './shared/footer-master/footer-master.component';
+import { HeaderMasterComponent } from './header-master/header-master.component';
+import { FooterMasterComponent } from './footer-master/footer-master.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TeamsComponent } from './teams/teams.component';
-import { MembersComponent } from './teams/members/members.component';
+import { MembersComponent } from './members/members.component';
 import { AppRoutingModule } from './app.routing.module';
 import { RouterModule } from '@angular/router';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
@@ -37,6 +38,7 @@ import { ErrorpageComponent } from './errorpage/errorpage.component';
     RouterModule,
     CommonModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from  '@angular/forms';
 
 @Component({
   selector: 'app-teams',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teams.component.css']
 })
 export class TeamsComponent implements OnInit {
+  myForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: [Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message:[ '', [Validators.required, Validators.maxLength(30)]]
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.name);
+    console.log('Email', form.value.email);
+    console.log('Message', form.value.message);
+  }
 }
