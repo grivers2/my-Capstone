0dff21c9412974a2764fb6dfc9b8c7f12a9be513
grivers24 <grivers24@gmail.com>
2022-09-23 15:12:26 -0400

    update assests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,16 @@ import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { CommonModule } from '@angular/common';
-
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { HeaderMasterComponent } from './header-master/header-master.component';
 import { FooterMasterComponent } from './footer-master/footer-master.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TeamsComponent } from './teams/teams.component';
 import { MembersComponent } from './members/members.component';
+
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app.routing.module';
 import { RouterModule } from '@angular/router';
-import { ErrorpageComponent } from './errorpage/errorpage.component';
-
 
 @NgModule({
   declarations: [
